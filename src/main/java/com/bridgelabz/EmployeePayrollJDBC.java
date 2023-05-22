f371e6d524fc17254dfd3f32635cc8030f2c17d1
Swapnil639 <swapnilpatil631997@gmail.com>
2023-05-22 09:48:47 +0530

    Added :Upadate the basic pay using prepare statements

--- src/main/java/com/bridgelabz/EmployeePayrollJDBC.java
+++ src/main/java/com/bridgelabz/EmployeePayrollJDBC.java
@@ -5,16 +5,18 @@ import java.sql.*;
 public class EmployeePayrollJDBC {
     public static void main(String[] args) throws SQLException {
         System.out.println("Welcome to Employee Payroll program on JDBC");
+        double salary = 312000.00;
+        int salary_id = 1;
         Connection connection=null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee_payroll", "root", "Swapnil123");
             System.out.println("Connection Done...");
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee");
-            while (resultSet.next()) {
-                System.out.println(resultSet.getInt("id") + " " + resultSet.getString("name"));
-            }
+            String sql = "update employee_salary set =salary? WHERE salary_id= ?";
+            PreparedStatement statement1 = connection.prepareStatement(sql);
+            statement1.setDouble(1, salary);
+            statement1.setInt(2, salary_id);
+            statement1.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
