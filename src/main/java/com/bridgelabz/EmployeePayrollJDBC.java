1611d9eb154c3c48fed40821b52d492328609d6f
Swapnil639 <swapnilpatil631997@gmail.com>
2023-05-22 09:25:48 +0530

    Added : DataBase connect to java sucessfully

--- src/main/java/com/bridgelabz/EmployeePayrollJDBC.java
+++ src/main/java/com/bridgelabz/EmployeePayrollJDBC.java
@@ -1,14 +1,22 @@
 package com.bridgelabz;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class EmployeePayrollJDBC {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Welcome to Employee Payroll program on JDBC");
+        Connection connection=null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee_payroll", "root", "Swapnil123");
+            System.out.println("Connection Done...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            connection.close();
+        }
 
     }
 }
