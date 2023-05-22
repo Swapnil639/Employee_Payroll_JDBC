c69656142925747fef0ca35d058627409445090c
Swapnil639 <swapnilpatil631997@gmail.com>
2023-05-22 09:07:24 +0530

    Added :Welcome message

---
+++ src/main/java/com/bridgelabz/EmployeePayrollJDBC.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class EmployeePayrollJDBC {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll program on JDBC");
+
+    }
+}
